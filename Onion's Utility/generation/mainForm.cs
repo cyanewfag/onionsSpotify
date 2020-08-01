094f6d91d33398e1d42dff7ae971f7919e3b358c
onion <62121953+cyanewfag@users.noreply.github.com>
2020-08-01 13:15:01 -0400

    Add files via upload

--- Onion's Utility/generation/mainForm.cs
+++ Onion's Utility/generation/mainForm.cs
@@ -45,7 +45,7 @@ namespace generation
                     clipboard += '\n' + @"   [ """ + fileNames[i].Replace(".wav", "").Split('\\').Last() + @""", ""unknown"", """ + fileNames[i].Split('\\').Last() + @""", " + Math.Round(GetWavFileDuration(fileNames[i]).TotalSeconds) + ", 0, false ]";
             }
 
-            Clipboard.SetText(clipboard + '\n' + "];");
+            Clipboard.SetText(clipboard + '\n' + "];" + "\n \n" + @"var options = [ " + '\n' + @"    [ ""onion_autoplay"", false ]," + '\n' + @"    [ ""onion_shuffle"", false ]," + '\n' + @"    [ ""onion_music_path"", """ + folderBrowserDialog1.SelectedPath.Replace(@"\", @"\\") + @"\\"" ]" + "\n ];");
         }
     }
 }
